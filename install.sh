8a3f7acc2de6a565f8a4626bc4cbaec1e331f4b8
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-06-30 06:51:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,15 @@ YELLOW='\033[0;33m' #YELLOW
 NC='\033[0m' # No Color
 
 sudo apt-get -y install libopus0 libasound2 libudev0 libavahi-client3 libcurl3 libevdev2
-printf "${YELLOW}Adding sound to use HDMI ${NC}\n"
-echo "hdmi_drive=2" | sudo sh -c 'cat >> /boot/config.txt'
+
+HDMIaudio=$(sudo cat /boot/config.txt|grep -i hdmi_drive|wc -l)
+	if [ "$HDMIaudio" -gt "2" ]; then
+		printf "${YELLOW}HDMI Sound already installed${NC}\n"
+	else
+		printf "${GREEN}Adding HDMI sound to configs{NC}\n"
+		echo "hdmi_drive=2" | sudo sh -c 'cat >> /boot/config.txt'
+	fi
+
 printf "${YELLOW}Adding moonlight into the sources list${NC}\n"
 echo "deb http://archive.itimmer.nl/raspbian/moonlight jessie main" | sudo sh -c 'cat >> /etc/apt/sources.list'
 sudo apt-key update
