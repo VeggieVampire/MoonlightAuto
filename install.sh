b5a675420adbc15bd6e4c15626b6698335541ae2
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-06-30 03:51:24 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y install libopus0 libasound2 libudev0 libavahi-client3 libcurl3 libevdev2
+echo "Adding sound to use HDMI"
+sudo echo 'hdmi_drive=2'>>/boot/config.txt
+echo "Adding moonlight into the sources list"
+echo "deb http://archive.itimmer.nl/raspbian/moonlight jessie main" | sudo sh -c 'cat >> /etc/apt/sources.list'
+sudo apt-key update
+sudo apt-get --force-yes -y install moonlight-embedded
