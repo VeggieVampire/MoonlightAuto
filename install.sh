eece7c3cbff5220c0b63355f961a83d1e41ee872
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-06-30 04:28:41 -0500

    adding color

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
+#
+# Author Nick Farrow
+#
+# Initial Release: 0
+
+RED='\033[0;31m'        #RED
+GREEN='\033[0;32m' #GREEN
+YELLOW='\033[0;33m' #YELLOW
+NC='\033[0m' # No Color
+
 sudo apt-get -y install libopus0 libasound2 libudev0 libavahi-client3 libcurl3 libevdev2
-echo "Adding sound to use HDMI"
+printf "${YELLOW}Adding sound to use HDMI ${NC}\n"
 echo "hdmi_drive=2" | sudo sh -c 'cat >> /boot/config.txt'
-echo "Adding moonlight into the sources list"
+printf "${YELLOW}Adding moonlight into the sources list${NC}\n"
 echo "deb http://archive.itimmer.nl/raspbian/moonlight jessie main" | sudo sh -c 'cat >> /etc/apt/sources.list'
 sudo apt-key update
 sudo apt-get -y update
 sudo apt-get --force-yes -y install moonlight-embedded
+printf "${GREEN}moonlight should be installed ${NC}\n"
+printf "${GREEN}type:moonlight ${NC}\n"
